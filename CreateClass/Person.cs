0f67691ee981c06c542767090702667a819e698f
tarakarcsi <tarakarcsi94@gmail.com>
2019-08-13 11:30:19 +0200

    createclass si exercise

---
+++ CreateClass/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    public enum Genders { Male, Female };
+
+    class Person
+    {
+        private string name;
+        private DateTime birtDate;
+        private Genders gender;
+        
+
+        public Person(string name, DateTime birtDate, Genders gender)
+        {
+            this.name = name;
+            this.birtDate = birtDate;
+            this.gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return "Person: " + name + " " + birtDate.ToString() + " " + gender;
+        }
+    }
+}
---
+++ CreateClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime p1Date = new DateTime(2000, 6, 20);
+            DateTime p2Date = new DateTime(2003, 2, 13);
+
+            Person person1 = new Person("Karoly", p1Date, Genders.Male);
+            Person person2 = new Person("Peter", p2Date, Genders.Male);
+
+            Console.WriteLine(person1);
+            Console.WriteLine(person2);
+            Console.ReadKey();
+        }
+    }
+}
